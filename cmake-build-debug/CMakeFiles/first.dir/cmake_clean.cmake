77078b2b7d505d1927a7c6610d0c88f833d63f62
yxxin111 <yx1509601684@163.com>
2020-09-16 23:48:37 +0800

    第一次提交

---
+++ cmake-build-debug/CMakeFiles/first.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/first.dir/main.cpp.obj"
+  "first.exe"
+  "first.exe.manifest"
+  "first.pdb"
+  "libfirst.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/first.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
